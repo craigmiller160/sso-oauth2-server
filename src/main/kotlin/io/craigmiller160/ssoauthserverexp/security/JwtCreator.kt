48ee43fba94b1e2965e26869e7cb2ed795d0c705
Craig Miller <craigmiller160@gmail.com>
2020-06-22 17:04:55 -0400

    Implemented the creation of the access token

--- src/main/kotlin/io/craigmiller160/ssoauthserverexp/security/JwtCreator.kt
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/security/JwtCreator.kt
@@ -1,15 +1,45 @@
 package io.craigmiller160.ssoauthserverexp.security
 
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
 import io.craigmiller160.ssoauthserverexp.config.TokenConfig
-import io.craigmiller160.ssoauthserverexp.entity.User
+import io.craigmiller160.ssoauthserverexp.util.LegacyDateConverter
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.util.Date
+import java.util.UUID
 
-// TODO delete if unnecessary
 @Component
-class JwtCreator(private val tokenConfig: TokenConfig) {
+class JwtCreator(
+        private val tokenConfig: TokenConfig,
+        private val legacyDateConverter: LegacyDateConverter
+) {
 
-    fun createJwt(user: User): String {
-        TODO("Finish this")
+    private fun generateExp(expSecs: Int): Date {
+        val now = LocalDateTime.now()
+        val exp = now.plusSeconds(expSecs.toLong())
+        return legacyDateConverter.convertLocalDateTimeToDate(exp)
+    }
+
+    fun createAccessToken(): String {
+        // TODO add claims
+        val claims = JWTClaimsSet.Builder()
+                .issueTime(Date())
+                .expirationTime(generateExp(tokenConfig.accessExpSecs))
+                .jwtID(UUID.randomUUID().toString())
+                .notBeforeTime(Date())
+                .build()
+
+        val header = JWSHeader.Builder(JWSAlgorithm.RS256)
+                .build()
+        val jwt = SignedJWT(header, claims)
+        val signer = RSASSASigner(tokenConfig.privateKey)
+
+        jwt.sign(signer)
+        return jwt.serialize()
     }
 
 }

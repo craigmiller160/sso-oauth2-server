c100c134af35d5d7be70d814c3f12bfbe1411dcc
Craig Miller <craigmiller160@gmail.com>
2020-06-22 14:34:29 -0400

    Some schema tweaks and wrote some entities

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
     id BIGSERIAL NOT NULL,
-    email VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    password VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
     id BIGSERIAL NOT NULL,
-    name VARCHAR(255),
+    name VARCHAR(255) UNIQUE NOT NULL,
     PRIMARY KEY (id)
 );
 
---
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/entity/Role.kt
@@ -0,0 +1,16 @@
+package io.craigmiller160.ssoauthserverexp.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "roles")
+data class Role (
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long,
+        val name: String
+)

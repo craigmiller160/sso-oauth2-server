460cec06b6bc5df2271eb53b883d621dbbf6abc6
Craig Miller <craigmiller160@gmail.com>
2020-06-22 15:23:34 -0400

    began working on OAuth2Controller

---
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/controller/OAuth2Controller.kt
@@ -0,0 +1,16 @@
+package io.craigmiller160.ssoauthserverexp.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/oauth")
+class OAuth2Controller {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello World"
+    }
+
+}

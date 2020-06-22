152defc157c60c39b5260b315f2b4239486e5f54
Craig Miller <craigmiller160@gmail.com>
2020-06-22 16:21:06 -0400

    Updated more security settings

--- src/main/kotlin/io/craigmiller160/ssoauthserverexp/config/WebSecurityConfig.kt
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/config/WebSecurityConfig.kt
@@ -1,14 +1,21 @@
 package io.craigmiller160.ssoauthserverexp.config
 
+import io.craigmiller160.ssoauthserverexp.service.ClientUserDetailsService
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class WebSecurityConfig(
+        private val clientUserDetailsService: ClientUserDetailsService
+) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
         http?.let {
@@ -25,7 +32,21 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                     .anyRequest().fullyAuthenticated()
                     .and()
                     .requiresChannel().anyRequest().requiresSecure()
+                    .and()
+                    .httpBasic()
         }
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth?.let {
+            auth.userDetailsService(clientUserDetailsService)
+                    .passwordEncoder(passwordEncoder())
+        }
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
     }
 
 }

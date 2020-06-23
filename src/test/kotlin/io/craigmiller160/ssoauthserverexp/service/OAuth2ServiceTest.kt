3952c17f19036e30447bf4ba1bcaa117c5d42693
Craig Miller <craigmiller160@gmail.com>
2020-06-23 19:59:01 -0400

    Updated OAuth2ServiceTest

--- src/test/kotlin/io/craigmiller160/ssoauthserverexp/service/OAuth2ServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/service/OAuth2ServiceTest.kt
@@ -1,28 +1,58 @@
 package io.craigmiller160.ssoauthserverexp.service
 
+import com.nhaarman.mockito_kotlin.isA
+import io.craigmiller160.ssoauthserverexp.dto.TokenResponse
+import io.craigmiller160.ssoauthserverexp.entity.RefreshToken
+import io.craigmiller160.ssoauthserverexp.repository.RefreshTokenRepository
+import io.craigmiller160.ssoauthserverexp.security.JwtCreator
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
 
+@ExtendWith(MockitoExtension::class)
 class OAuth2ServiceTest {
 
+    @Mock
+    private lateinit var jwtCreator: JwtCreator
+    @Mock
+    private lateinit var refreshTokenRepo: RefreshTokenRepository
+
+    @InjectMocks
     private lateinit var oAuth2Service: OAuth2Service
+
+    private val accessToken = "AccessToken"
+    private val refreshToken = "RefreshToken"
 
     @Test
     fun test_clientCredentials() {
+        `when`(jwtCreator.createAccessToken())
+                .thenReturn(accessToken)
+        `when`(jwtCreator.createRefreshToken())
+                .thenReturn(refreshToken)
+
         val result = oAuth2Service.clientCredentials()
-        assertEquals("Client Credentials", result)
+        assertEquals(TokenResponse(accessToken, refreshToken), result)
+
+        verify(refreshTokenRepo, times(1))
+                .save(isA<RefreshToken>())
     }
 
     @Test
     fun test_password() {
         val result = oAuth2Service.password()
-        assertEquals("Password", result)
+        assertEquals(TokenResponse("password", ""), result)
     }
 
     @Test
     fun test_authCode() {
         val result = oAuth2Service.authCode()
-        assertEquals("Auth Code", result)
+        assertEquals(TokenResponse("authCode", ""), result)
     }
 
 }

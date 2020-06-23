8099dd06fef45f76d772976809340a65b88579e4
Craig Miller <craigmiller160@gmail.com>
2020-06-23 17:30:40 -0400

    Stubbed out ClientUserDetailsServiceTest

--- src/test/kotlin/io/craigmiller160/ssoauthserverexp/service/ClientUserDetailsServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/service/ClientUserDetailsServiceTest.kt
@@ -1,12 +1,30 @@
-//package io.craigmiller160.ssoauthserverexp.service
-//
-//import org.junit.jupiter.api.Test
-//
-//class ClientUserDetailsServiceTest {
-//
-//    @Test
-//    fun test() {
-//        TODO("Finish this")
-//    }
-//
-//}
+package io.craigmiller160.ssoauthserverexp.service
+
+import io.craigmiller160.ssoauthserverexp.repository.ClientRepository
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class ClientUserDetailsServiceTest {
+
+    @Mock
+    private lateinit var clientRepo: ClientRepository
+
+    @Test
+    fun test_loadUserByUsername() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_loadUserByUsername_noClientKey() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_loadUserByUsername_userNotFound() {
+        TODO("Finish this")
+    }
+
+}

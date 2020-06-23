5dce82690793598159e949d2d8ca0f4b137c5659
Craig Miller <craigmiller160@gmail.com>
2020-06-23 11:09:52 -0400

    Stubbed out actual tests for TokenConfigTest

--- src/test/kotlin/io/craigmiller160/ssoauthserverexp/config/TokenConfigTest.kt
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/config/TokenConfigTest.kt
@@ -4,8 +4,25 @@ import org.junit.jupiter.api.Test
 
 class TokenConfigTest {
 
+    private val accessExpSecs = 10
+    private val refreshExpSecs = 20
+    private val keyStorePath = "keyStorePath"
+    private val keyStoreType = "type"
+    private val keyStorePassword = "pass"
+    private val keyStoreAlias = "alias"
+
     @Test
-    fun test() {
+    fun test_loadKeys_classpath() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_loadKeys_classpath_notFound() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_loadKeys_file() {
         TODO("Finish this")
     }
 

429a8fd597fad41c50b2ac6871714fc40d1c285d
Craig Miller <craigmiller160@gmail.com>
2020-06-23 16:31:58 -0400

    Stubbed out JwtCreatorTest

--- src/test/kotlin/io/craigmiller160/ssoauthserverexp/security/JwtCreatorTest.kt
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/security/JwtCreatorTest.kt
@@ -1,12 +1,32 @@
-//package io.craigmiller160.ssoauthserverexp.security
-//
-//import org.junit.jupiter.api.Test
-//
-//class JwtCreatorTest {
-//
-//    @Test
-//    fun test() {
-//        TODO("Finish this")
-//    }
-//
-//}
+package io.craigmiller160.ssoauthserverexp.security
+
+import io.craigmiller160.ssoauthserverexp.config.TokenConfig
+import io.craigmiller160.ssoauthserverexp.util.LegacyDateConverter
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class JwtCreatorTest {
+
+    @Mock
+    private lateinit var tokenConfig: TokenConfig
+    @Mock
+    private lateinit var legacyDateConverter: LegacyDateConverter
+
+    @InjectMocks
+    private lateinit var jwtCreator: JwtCreator
+
+    @Test
+    fun test_createAccessToken() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_createRefreshToken() {
+        TODO("Finish this")
+    }
+
+}

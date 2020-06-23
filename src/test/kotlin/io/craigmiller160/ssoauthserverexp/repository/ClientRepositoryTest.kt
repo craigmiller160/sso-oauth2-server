0eb4bd59ec3afa9a8e3edf7c5e9b35d718940ca6
Craig Miller <craigmiller160@gmail.com>
2020-06-23 15:45:23 -0400

    Working on getting more tests to work

--- src/test/kotlin/io/craigmiller160/ssoauthserverexp/repository/ClientRepositoryTest.kt
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/repository/ClientRepositoryTest.kt
@@ -1,14 +1,19 @@
 package io.craigmiller160.ssoauthserverexp.repository
 
+import io.craigmiller160.ssoauthserverexp.entity.Client
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@DataJpaTest
 class ClientRepositoryTest {
 
     @Autowired
@@ -16,7 +21,7 @@ class ClientRepositoryTest {
 
     @Before
     fun setup() {
-        TODO("Setup DB")
+//        println(clientRepo) // TODO delete this
     }
 
     @Test

6dcdcdaf452f24c44ca6ffbff471e2097818b12c
Craig <craigmiller160@gmail.com>
2020-06-23 13:39:49 -0400

    Stubbed out ClientRepositoryTest

--- src/test/kotlin/io/craigmiller160/ssoauthserverexp/repository/ClientRepositoryTest.kt
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/repository/ClientRepositoryTest.kt
@@ -1,11 +1,31 @@
 package io.craigmiller160.ssoauthserverexp.repository
 
+import org.junit.Before
 import org.junit.jupiter.api.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
+@RunWith(SpringJUnit4ClassRunner::class)
+@SpringBootTest
 class ClientRepositoryTest {
 
+    @Autowired
+    private lateinit var clientRepo: ClientRepository
+
+    @Before
+    fun setup() {
+        TODO("Setup DB")
+    }
+
     @Test
-    fun test() {
+    fun test_findByClientKey() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_findByClientKey_noResults() {
         TODO("Finish this")
     }
 

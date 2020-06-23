a624bd23d9ad5d27c799751b5d69024014fd1343
Craig Miller <craigmiller160@gmail.com>
2020-06-23 17:52:50 -0400

    Added logging to refresh token cleanup.

--- src/main/kotlin/io/craigmiller160/ssoauthserverexp/service/RefreshTokenCleanupService.kt
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/service/RefreshTokenCleanupService.kt
@@ -2,6 +2,8 @@ package io.craigmiller160.ssoauthserverexp.service
 
 import io.craigmiller160.ssoauthserverexp.config.TokenConfig
 import io.craigmiller160.ssoauthserverexp.repository.RefreshTokenRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -12,8 +14,11 @@ class RefreshTokenCleanupService (
         private val tokenConfig: TokenConfig
 ) {
 
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
     @Scheduled(fixedRate = 1 * 60 * 60 * 1000) // Run every hour
     fun cleanupRefreshTokens() {
+        log.info("Cleaning up refresh tokens older than ${tokenConfig.deleteOlderThanSecs} seconds")
         val maxTimestamp = LocalDateTime.now().minusSeconds(tokenConfig.deleteOlderThanSecs)
         refreshTokenRepo.removeOldTokens(maxTimestamp)
     }

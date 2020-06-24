1a394a83c7ce1ed54a31931be8d28d77612ea62c
Craig Miller <craigmiller160@gmail.com>
2020-06-24 14:18:06 -0400

    Fixed another part of OAuth2ServiceTest

--- src/test/kotlin/io/craigmiller160/ssoauthserverexp/service/OAuth2ServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/service/OAuth2ServiceTest.kt
@@ -2,12 +2,16 @@ package io.craigmiller160.ssoauthserverexp.service
 
 import com.nhaarman.mockito_kotlin.isA
 import io.craigmiller160.ssoauthserverexp.dto.TokenResponse
+import io.craigmiller160.ssoauthserverexp.entity.Client
 import io.craigmiller160.ssoauthserverexp.entity.RefreshToken
 import io.craigmiller160.ssoauthserverexp.repository.RefreshTokenRepository
 import io.craigmiller160.ssoauthserverexp.repository.RoleRepository
 import io.craigmiller160.ssoauthserverexp.repository.UserRepository
+import io.craigmiller160.ssoauthserverexp.security.ClientUserDetails
 import io.craigmiller160.ssoauthserverexp.security.JwtCreator
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
@@ -16,6 +20,9 @@ import org.mockito.Mockito.`when`
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @ExtendWith(MockitoExtension::class)
@@ -31,6 +38,10 @@ class OAuth2ServiceTest {
     private lateinit var roleRepo: RoleRepository
     @Mock
     private lateinit var passwordEncoder: PasswordEncoder
+    @Mock
+    private lateinit var securityContext: SecurityContext
+    @Mock
+    private lateinit var authentication: Authentication
 
     @InjectMocks
     private lateinit var oAuth2Service: OAuth2Service
@@ -38,10 +49,40 @@ class OAuth2ServiceTest {
     private val accessToken = "AccessToken"
     private val refreshToken = "RefreshToken"
 
+    private val client = Client(
+            id = 1L,
+            name = "Name",
+            clientKey = "Key",
+            clientSecret = "Secret",
+            enabled = true,
+            allowClientCredentials = true,
+            allowAuthCode = true,
+            allowPassword = true
+    )
+    private val clientUserDetails = ClientUserDetails(client)
+
+    private fun setupSecurityContext() {
+        `when`(securityContext.authentication)
+                .thenReturn(authentication)
+        `when`(authentication.principal)
+                .thenReturn(clientUserDetails)
+    }
+
+    @BeforeEach
+    fun setup() {
+        SecurityContextHolder.setContext(securityContext)
+    }
+
+    @AfterEach
+    fun clean() {
+        SecurityContextHolder.clearContext()
+    }
+
     @Test
     fun test_clientCredentials() {
-//        `when`(jwtCreator.createAccessToken())
-//                .thenReturn(accessToken)
+        setupSecurityContext()
+        `when`(jwtCreator.createAccessToken(clientUserDetails))
+                .thenReturn(accessToken)
         `when`(jwtCreator.createRefreshToken())
                 .thenReturn(refreshToken)
 
@@ -50,7 +91,6 @@ class OAuth2ServiceTest {
 
         verify(refreshTokenRepo, times(1))
                 .save(isA<RefreshToken>())
-        TODO("Finish this")
     }
 
     @Test

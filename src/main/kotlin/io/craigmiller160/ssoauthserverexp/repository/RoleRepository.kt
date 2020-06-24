8286f32b0d4ae693c9328cb4a24d2e4b902743ee
Craig Miller <craigmiller160@gmail.com>
2020-06-24 13:00:16 -0400

    Added important RoleRepository method and stubbed out tests

--- src/main/kotlin/io/craigmiller160/ssoauthserverexp/repository/RoleRepository.kt
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/repository/RoleRepository.kt
@@ -2,7 +2,22 @@ package io.craigmiller160.ssoauthserverexp.repository
 
 import io.craigmiller160.ssoauthserverexp.entity.Role
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RoleRepository : JpaRepository<Role,Long>
+interface RoleRepository : JpaRepository<Role,Long> {
+
+    @Query("""
+        SELECT r
+        FROM Role r
+        WHERE r.id IN (
+            SELECT cur.roleId
+            FROM ClientUserRole cur
+            WHERE cur.userId = :userId
+            AND cur.clientId = :clientId
+        )
+    """)
+    fun findAllByUserIdAndClientId(userId: Long, clientId: Long): List<Role>
+
+}
---
+++ src/test/kotlin/io/craigmiller160/ssoauthserverexp/repository/RoleRepositoryTest.kt
@@ -0,0 +1,14 @@
+package io.craigmiller160.ssoauthserverexp.repository
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+class RoleRepositoryTest {
+
+    @Test
+    fun test_findAllByUserIdAndClientId() {
+        TODO("Finish this")
+    }
+
+}

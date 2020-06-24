f29e39b6a593a07ffcf526855853819323266c63
Craig Miller <craigmiller160@gmail.com>
2020-06-24 14:01:36 -0400

    Fixed some exception issues

--- src/main/kotlin/io/craigmiller160/ssoauthserverexp/exception/InvalidLoginException.kt
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/exception/InvalidLoginException.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.ssoauthserverexp.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.RuntimeException
 
 @ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "Invalid login credentials")
-class InvalidLoginException(msg: String) : Exception(msg)
+class InvalidLoginException(msg: String) : RuntimeException(msg)
--- src/main/kotlin/io/craigmiller160/ssoauthserverexp/exception/UnsupportedGrantTypeException.kt
+++ src/main/kotlin/io/craigmiller160/ssoauthserverexp/exception/UnsupportedGrantTypeException.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.ssoauthserverexp.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.RuntimeException
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Unsupported OAuth2 Grant Type")
-class UnsupportedGrantTypeException(msg: String) : Exception(msg)
+class UnsupportedGrantTypeException(msg: String) : RuntimeException(msg)

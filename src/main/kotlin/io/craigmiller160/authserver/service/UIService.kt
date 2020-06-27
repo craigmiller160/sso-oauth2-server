aa686c1cbc8b2024bbb9aca2190fc82449131356
Craig Miller <craigmiller160@gmail.com>
2020-06-27 17:42:31 -0400

    Code cleanup

--- src/main/kotlin/io/craigmiller160/authserver/service/UIService.kt
+++ src/main/kotlin/io/craigmiller160/authserver/service/UIService.kt
@@ -1,8 +1,10 @@
 package io.craigmiller160.authserver.service
 
+import io.craigmiller160.authserver.dto.PageRequest
 import io.craigmiller160.authserver.repository.ClientRepository
-import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Service
 
+@Service
 class UIService (
         private val clientRepo: ClientRepository
 ) {

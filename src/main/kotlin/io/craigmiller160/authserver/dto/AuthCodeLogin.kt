ec714ee67d5f45b777994261d7977e14758efa76
Craig Miller <craigmiller160@gmail.com>
2020-06-27 17:01:28 -0400

    Got the clientId and redirectUri sent to the server

--- src/main/kotlin/io/craigmiller160/authserver/dto/AuthCodeLogin.kt
+++ src/main/kotlin/io/craigmiller160/authserver/dto/AuthCodeLogin.kt
@@ -2,5 +2,7 @@ package io.craigmiller160.authserver.dto
 
 data class AuthCodeLogin (
         val username: String,
-        val password: String
+        val password: String,
+        var clientId: String,
+        var redirectUri: String
 )

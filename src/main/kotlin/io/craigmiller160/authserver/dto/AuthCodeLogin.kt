0fa03c80bc47ae4d2f66d40e7eb189b6e103f989
Craig Miller <craigmiller160@gmail.com>
2020-06-28 09:43:40 -0400

    Added responseType to AuthCodeLogin

--- src/main/kotlin/io/craigmiller160/authserver/dto/AuthCodeLogin.kt
+++ src/main/kotlin/io/craigmiller160/authserver/dto/AuthCodeLogin.kt
@@ -3,6 +3,7 @@ package io.craigmiller160.authserver.dto
 data class AuthCodeLogin (
         val username: String,
         val password: String,
-        var clientId: String,
-        var redirectUri: String
+        val clientId: String,
+        val redirectUri: String,
+        val responseType: String
 )

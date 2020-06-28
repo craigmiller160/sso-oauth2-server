2c5c144eecd6a4e0d767356a45653281d6ac3dbf
Craig Miller <craigmiller160@gmail.com>
2020-06-28 18:00:41 -0400

    Updated test data

--- sql/data.sql
+++ sql/data.sql
@@ -6,14 +6,15 @@ VALUES (1, 'craig@gmail.com', 'Craig', 'Miller', '{bcrypt}$2a$10$HYKpEK6BFUFH99f
 INSERT INTO dev.clients (id, name, client_key, client_secret, enabled, allow_client_credentials, allow_password, allow_auth_code, access_token_timeout_secs, refresh_token_timeout_secs, auth_code_timeout_secs, redirect_uri)
 VALUES (1, 'ClientCredsApp', 'client_creds', '{bcrypt}$2a$10$HYKpEK6BFUFH99fHm5yOhuk4hn1gFErtLveeonVSHW1G7n5bUhGUe', true, true, false, false, 300, 3600, null, null),
        (2, 'PasswordApp', 'password', '{bcrypt}$2a$10$HYKpEK6BFUFH99fHm5yOhuk4hn1gFErtLveeonVSHW1G7n5bUhGUe', true, false, true, false, 300, 3600, null, null),
-       (3, 'AuthCodeApp', 'auth_code', '{bcrypt}$2a$10$HYKpEK6BFUFH99fHm5yOhuk4hn1gFErtLveeonVSHW1G7n5bUhGUe', true, false, false, true, 300, 3600, 60, 'http://somewhere.com');
+       (3, 'AuthCodeApp', 'auth_code', '{bcrypt}$2a$10$HYKpEK6BFUFH99fHm5yOhuk4hn1gFErtLveeonVSHW1G7n5bUhGUe', true, false, false, true, 300, 3600, 60, 'http://somewhere.com'),
+       (4, 'auth-management-service', 'a4cc4fef-564e-44c1-82af-45572f124c1a', '', true, false, true, true, 300, 3600, 60, 'https://localhost:7004/api/authcode/code');
 
 INSERT INTO dev.roles (id, name, client_id)
 VALUES (1, 'ROLE_READ', 2),
        (2, 'ROLE_WRITE', 2);
 
 INSERT INTO dev.client_users (id, user_id, client_id)
-VALUES (1, 1, 2), (2, 3, 3);
+VALUES (1, 1, 2), (2, 3, 3), (3, 1, 4);
 
 INSERT INTO dev.client_user_roles (user_id, client_id, role_id)
 VALUES (1, 2, 1), (1, 2, 2);

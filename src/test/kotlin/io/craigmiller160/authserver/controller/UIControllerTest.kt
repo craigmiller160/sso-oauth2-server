8d2e6b42b317258d6f34ad7a0f81baa2b0bb4a98
Craig Miller <craigmiller160@gmail.com>
2020-07-02 18:06:18 -0400

    Stubbed out tests

--- src/test/kotlin/io/craigmiller160/authserver/controller/UIControllerTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/controller/UIControllerTest.kt
@@ -1,11 +1,38 @@
 package io.craigmiller160.authserver.controller
 
+import io.craigmiller160.authserver.service.UIService
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
 
+@ExtendWith(MockitoExtension::class)
 class UIControllerTest {
 
+    @Mock
+    private lateinit var uiService: UIService
+
+    @InjectMocks
+    private lateinit var uiController: UIController
+
     @Test
-    fun test() {
+    fun test_getCss() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_getCss_404() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_getPage() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_getPage_404() {
         TODO("Finish this")
     }
 

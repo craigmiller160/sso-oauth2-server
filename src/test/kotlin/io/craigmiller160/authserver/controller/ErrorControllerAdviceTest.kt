6a872bc6e94e5a33dea54d6b0810820240c50fd0
Craig Miller <craigmiller160@gmail.com>
2020-07-06 16:08:17 -0400

    Finished ErrorControllerAdviceTest

--- src/test/kotlin/io/craigmiller160/authserver/controller/ErrorControllerAdviceTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/controller/ErrorControllerAdviceTest.kt
@@ -6,6 +6,7 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.hasProperty
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
@@ -13,6 +14,8 @@ import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.security.access.AccessDeniedException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import javax.servlet.http.HttpServletRequest
 
 @ExtendWith(MockitoExtension::class)
@@ -29,6 +32,7 @@ class ErrorControllerAdviceTest {
         val ex = UnsupportedGrantTypeException("message")
 
         val error = errorControllerAdvice.exception(req, ex)
+        assertEquals(400, error.statusCodeValue)
         assertThat(error.body, allOf(
                 hasProperty("status", equalTo(400)),
                 hasProperty("error", equalTo("Bad Request")),
@@ -44,6 +48,7 @@ class ErrorControllerAdviceTest {
         val ex = Exception("message")
 
         val error = errorControllerAdvice.exception(req, ex)
+        assertEquals(500, error.statusCodeValue)
         assertThat(error.body, allOf(
                 hasProperty("status", equalTo(500)),
                 hasProperty("error", equalTo("Internal Server Error")),
@@ -60,6 +65,7 @@ class ErrorControllerAdviceTest {
         `when`(ex.message).thenReturn("message")
 
         val error = errorControllerAdvice.accessDeniedException(req, ex)
+        assertEquals(403, error.statusCodeValue)
         assertThat(error.body, allOf(
                 hasProperty("status", equalTo(403)),
                 hasProperty("error", equalTo("Access Denied")),
@@ -71,12 +77,36 @@ class ErrorControllerAdviceTest {
 
     @Test
     fun test_mediaTypeNotSupportedException() {
-        TODO("Finish this")
+        `when`(req.requestURI).thenReturn("uri")
+        val ex = Mockito.mock(HttpMediaTypeNotSupportedException::class.java)
+        `when`(ex.message).thenReturn("message")
+
+        val error = errorControllerAdvice.mediaTypeNotSupportedException(req, ex)
+        assertEquals(415, error.statusCodeValue)
+        assertThat(error.body, allOf(
+                hasProperty("status", equalTo(415)),
+                hasProperty("error", equalTo("Unsupported Media Type")),
+                hasProperty("message", equalTo("message")),
+                hasProperty("timestamp", notNullValue()),
+                hasProperty("path", equalTo("uri"))
+        ))
     }
 
     @Test
     fun test_methodNotSupportedException() {
-        TODO("Finish this")
+        `when`(req.requestURI).thenReturn("uri")
+        val ex = Mockito.mock(HttpRequestMethodNotSupportedException::class.java)
+        `when`(ex.message).thenReturn("message")
+
+        val error = errorControllerAdvice.methodNotSupportedException(req, ex)
+        assertEquals(405, error.statusCodeValue)
+        assertThat(error.body, allOf(
+                hasProperty("status", equalTo(405)),
+                hasProperty("error", equalTo("Method Not Allowed")),
+                hasProperty("message", equalTo("message")),
+                hasProperty("timestamp", notNullValue()),
+                hasProperty("path", equalTo("uri"))
+        ))
     }
 
 }

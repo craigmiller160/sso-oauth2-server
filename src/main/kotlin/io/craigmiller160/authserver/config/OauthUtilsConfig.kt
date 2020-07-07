81f1a48083402705a08a5242dc2fc1c3495f9209
Craig Miller <craigmiller160@gmail.com>
2020-07-07 17:29:19 -0400

    Code cleanup

---
+++ src/main/kotlin/io/craigmiller160/authserver/config/OauthUtilsConfig.kt
@@ -0,0 +1,10 @@
+package io.craigmiller160.authserver.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan(basePackages = [
+    "io.craigmiller160.oauth.utils.controller"
+])
+class OauthUtilsConfig
--- src/main/kotlin/io/craigmiller160/authserver/config/WebConfig.kt
+++ src/main/kotlin/io/craigmiller160/authserver/config/WebConfig.kt
@@ -1,20 +1,16 @@
 package io.craigmiller160.authserver.config
 
-import io.craigmiller160.authserver.controller.NewControllerLogging
 import io.craigmiller160.oauth.utils.controller.RequestLogger
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@Import(RequestLogger::class)
 class WebConfig (
         private val requestLogger: RequestLogger
 ) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
-        println("AddInterceptor: $requestLogger") // TODO delete this
         registry.addInterceptor(requestLogger)
                 .addPathPatterns("/**/**")
     }

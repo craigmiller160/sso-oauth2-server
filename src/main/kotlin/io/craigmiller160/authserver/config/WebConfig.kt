b6a83606891223b6000f5a161ea4e70ec7a95821
Craig Miller <craigmiller160@gmail.com>
2020-07-07 17:24:40 -0400

    Integrated logging solution from the library

--- src/main/kotlin/io/craigmiller160/authserver/config/WebConfig.kt
+++ src/main/kotlin/io/craigmiller160/authserver/config/WebConfig.kt
@@ -1,17 +1,21 @@
 package io.craigmiller160.authserver.config
 
 import io.craigmiller160.authserver.controller.NewControllerLogging
+import io.craigmiller160.oauth.utils.controller.RequestLogger
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@Import(RequestLogger::class)
 class WebConfig (
-        private val newControllerLogging: NewControllerLogging
+        private val requestLogger: RequestLogger
 ) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(newControllerLogging)
+        println("AddInterceptor: $requestLogger") // TODO delete this
+        registry.addInterceptor(requestLogger)
                 .addPathPatterns("/**/**")
     }
 

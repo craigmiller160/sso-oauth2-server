9b617c30d78de84ea5d6b1f15bcc589e4920a643
Craig Miller <craigmiller160@gmail.com>
2020-08-05 16:18:30 -0400

    Stubbed out all refresh token grant tests

--- src/test/kotlin/io/craigmiller160/authserver/integration/oAuth2Controller/TokenRefreshIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/integration/oAuth2Controller/TokenRefreshIntegrationTest.kt
@@ -10,7 +10,37 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 class TokenRefreshIntegrationTest {
 
     @Test
-    fun test() {
+    fun `token() - successful refresh_token grant`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `token() - successful refresh_token grant with user`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `token() - refresh_token grant without refresh_token property`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `token() - refresh_token grant without DB token`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `token() - refresh_token grant with bad signature`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `token() - refresh_token grant with expired token`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `token() - refresh_token grant with bad client ID`() {
         TODO("Finish this")
     }
 

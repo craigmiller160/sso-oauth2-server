11dd5a67bb38a1ee9b01b147b317984dedb8c5c1
Craig Miller <craigmiller160@gmail.com>
2020-08-05 16:04:26 -0400

    Began stubbing out tests

---
+++ src/test/kotlin/io/craigmiller160/authserver/integration/OAuth2AuthCodeLoginIntegrationTest.kt
@@ -0,0 +1,47 @@
+package io.craigmiller160.authserver.integration
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+class OAuth2AuthCodeLoginIntegrationTest : AbstractControllerIntegrationTest() {
+
+    @Test
+    fun test_authCodeLogin() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_authCodeLogin_missingState() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_authCodeLogin_badResponseType() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_authCodeLogin_noClient() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_authCodeLogin_noAuthCode() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_authCodeLogin_noUser() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun test_authCodeLogin_badCreds() {
+        TODO("Finish this")
+    }
+
+}

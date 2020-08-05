b83feb9e17b18de81800a5c0b08cd27d0d61d997
Craig Miller <craigmiller160@gmail.com>
2020-08-05 16:28:05 -0400

    Stubbed out test setup

--- src/test/kotlin/io/craigmiller160/authserver/integration/oAuth2Controller/TokenClientCredentialsIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/integration/oAuth2Controller/TokenClientCredentialsIntegrationTest.kt
@@ -1,13 +1,36 @@
 package io.craigmiller160.authserver.integration.oAuth2Controller
 
+import io.craigmiller160.authserver.entity.Client
+import io.craigmiller160.authserver.integration.AbstractControllerIntegrationTest
+import io.craigmiller160.authserver.repository.ClientRepository
+import io.craigmiller160.authserver.testutils.TestData
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
-class TokenClientCredentialsIntegrationTest {
+class TokenClientCredentialsIntegrationTest : AbstractControllerIntegrationTest() {
+
+    @Autowired
+    private lateinit var clientRepo: ClientRepository
+
+    private lateinit var client1: Client
+
+    @BeforeEach
+    fun setup() {
+        client1 = TestData.createClient()
+        client1 = clientRepo.save(client1)
+    }
+
+    @AfterEach
+    fun clean() {
+        clientRepo.deleteAll()
+    }
 
     @Test
     fun `token() - client_credentials grant invalid client header`() {

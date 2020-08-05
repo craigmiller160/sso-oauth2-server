a8ce793088118f725a81e1611fd60a9626808e30
Craig Miller <craigmiller160@gmail.com>
2020-08-05 15:57:35 -0400

    Got more tests working

--- src/test/kotlin/io/craigmiller160/authserver/integration/UIControllerIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/integration/UIControllerIntegrationTest.kt
@@ -1,14 +1,37 @@
 package io.craigmiller160.authserver.integration
 
+import io.craigmiller160.authserver.entity.Client
+import io.craigmiller160.authserver.repository.ClientRepository
+import io.craigmiller160.authserver.testutils.TestData
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 class UIControllerIntegrationTest : AbstractControllerIntegrationTest() {
+
+    @Autowired
+    private lateinit var clientRepo: ClientRepository
+
+    private lateinit var client1: Client
+    private lateinit var client2: Client
+
+    @BeforeEach
+    fun setup() {
+        client1 = TestData.createClient()
+        client2 = TestData.createClient().copy(
+                clientKey = "key2",
+                allowAuthCode = false
+        )
+
+        client1 = clientRepo.save(client1)
+        client2 = clientRepo.save(client2)
+    }
 
     @Test
     fun test_getCss_bootstrap() {
@@ -28,22 +51,59 @@ class UIControllerIntegrationTest : AbstractControllerIntegrationTest() {
 
     @Test
     fun test_getCss_other() {
-        TODO("Finish this")
+        apiProcessor.call {
+            request {
+                path = "/ui/resources/css/other.min.css"
+            }
+            response {
+                status = 404
+            }
+        }
     }
 
     @Test
     fun test_getPage_login() {
+        val result = apiProcessor.call {
+            request {
+                path = "/ui/login.html?client_id=${client1.clientKey}&redirect_uri=${client1.redirectUri}&response_type=code"
+            }
+            response {
+                headers = mapOf(
+                        "Content-Type" to "text/html"
+                )
+            }
+        }
+
+        assertTrue(result.response.contentAsString.isNotBlank())
+    }
+
+    @Test
+    fun test_getPage_noAuthCode() {
         TODO("Finish this")
     }
 
     @Test
     fun test_getPage_other() {
-       TODO("Finish this")
+        apiProcessor.call {
+            request {
+                path = "/ui/foo.html"
+            }
+            response {
+                status = 404
+            }
+        }
     }
 
     @Test
     fun test_getPage_badParams() {
-        TODO("Finish this")
+        apiProcessor.call {
+            request {
+                path = "/ui/login.html"
+            }
+            response {
+                status = 401
+            }
+        }
     }
 
 }

3ee2c3194f3e8a35c6fa35bcde0f0949785f5d19
Craig Miller <craigmiller160@gmail.com>
2020-08-08 19:59:03 -0400

    Working on some of the tests

--- src/test/kotlin/io/craigmiller160/authserver/integration/oAuth2Controller/TokenAuthCodeIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/integration/oAuth2Controller/TokenAuthCodeIntegrationTest.kt
@@ -1,17 +1,54 @@
 package io.craigmiller160.authserver.integration.oAuth2Controller
 
+import io.craigmiller160.apitestprocessor.body.formOf
+import io.craigmiller160.apitestprocessor.config.AuthType
+import io.craigmiller160.authserver.integration.AbstractControllerIntegrationTest
+import io.craigmiller160.authserver.security.GrantType
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpMethod
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
-class TokenAuthCodeIntegrationTest {
+class TokenAuthCodeIntegrationTest : AbstractControllerIntegrationTest() {
+
+    private fun createTokenForm() = formOf(
+            "grant_type" to GrantType.AUTH_CODE,
+            "client_id" to validClientKey,
+            "code" to "todo",
+            "redirect_uri" to authClient.redirectUri!!
+    )
 
     @Test
     fun `token() - auth_code grant invalid client header`() {
-        TODO("Finish this")
+        apiProcessor.call {
+            request {
+                path = "/oauth/token"
+                method = HttpMethod.POST
+                body = createTokenForm()
+                overrideAuth {
+                    type = AuthType.BASIC
+                    userName = "abc"
+                    password = "def"
+                }
+            }
+            response {
+                status = 401
+            }
+        }
+    }
+
+    @Test
+    fun `test() - auth_code grant success`() {
+        apiProcessor.call {
+            request {
+                path = "/oauth/token"
+                method = HttpMethod.POST
+                body = createTokenForm()
+            }
+        }
     }
 
     @Test
@@ -46,11 +83,6 @@ class TokenAuthCodeIntegrationTest {
 
     @Test
     fun `test() - auth_code grant with expired auth code`() {
-        TODO("Finish this")
-    }
-
-    @Test
-    fun `test() - auth_code grant success`() {
         TODO("Finish this")
     }
 

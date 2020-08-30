4dff5fa74dd7b640ae3b510065ab0e7ce8665203
Craig Miller <craigmiller160@gmail.com>
2020-08-29 20:08:24 -0400

    More sql changes and JPA entity changes

--- sql/migrations/1.1.0/redirect_uris.sql
+++ sql/migrations/1.1.0/redirect_uris.sql
@@ -2,6 +2,9 @@
 ALTER TABLE clients
 DROP COLUMN redirect_uri;
 
+ALTER TABLE clients
+ALTER COLUMN auth_code_timeout_secs SET NOT NULL;
+
 CREATE TABLE client_redirect_uris (
     id BIGSERIAL NOT NULL,
     client_id BIGINT NOT NULL,
--- src/main/kotlin/io/craigmiller160/authserver/entity/Client.kt
+++ src/main/kotlin/io/craigmiller160/authserver/entity/Client.kt
@@ -1,9 +1,12 @@
 package io.craigmiller160.authserver.entity
 
+import javax.persistence.CascadeType
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
@@ -18,11 +21,20 @@ data class Client (
         val enabled: Boolean,
         val accessTokenTimeoutSecs: Int,
         val refreshTokenTimeoutSecs: Int,
-        val authCodeTimeoutSecs: Int?,
-        val redirectUri: String?
+        val authCodeTimeoutSecs: Int,
+
+        @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
+        @JoinColumn(name = "client_id")
+        val clientRedirectUris: List<ClientRedirectUri>
 ) {
+
+        fun getRedirectUris(): List<String> {
+                return clientRedirectUris.map { it.redirectUri }
+        }
+
         fun supportsAuthCode(otherRedirectUri: String): Boolean {
-                return authCodeTimeoutSecs != null &&
-                        redirectUri != null && redirectUri == otherRedirectUri
+                TODO("Finish this")
+//                return authCodeTimeoutSecs != null &&
+//                        redirectUri != null && redirectUri == otherRedirectUri
         }
 }

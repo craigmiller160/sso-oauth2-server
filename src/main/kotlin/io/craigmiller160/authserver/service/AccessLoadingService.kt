492939114a2613e3c30c1ad08fc4dccc2518709f
Craig Miller <craigmiller160@gmail.com>
2022-02-21 18:42:34 -0500

    More progress with refactor

--- src/main/kotlin/io/craigmiller160/authserver/service/AccessLoadingService.kt
+++ src/main/kotlin/io/craigmiller160/authserver/service/AccessLoadingService.kt
@@ -6,6 +6,8 @@ import arrow.core.computations.either
 import arrow.core.flatMap
 import io.craigmiller160.authserver.dto.access.ClientWithRolesAccess
 import io.craigmiller160.authserver.dto.access.UserWithClientsAccess
+import io.craigmiller160.authserver.entity.Client
+import io.craigmiller160.authserver.entity.Role
 import io.craigmiller160.authserver.entity.User
 import io.craigmiller160.authserver.exception.AccessNotFoundException
 import io.craigmiller160.authserver.repository.ClientRepository
@@ -22,6 +24,26 @@ private fun createGetUserById(userRepo: UserRepository): (Long) -> Either<Throwa
                     }
 }
 
+private fun createGetClients(clientRepo: ClientRepository): (Long,Long?) -> Either<Throwable,List<Client>> {
+    return fun(userId: Long, clientId: Long?): Either<Throwable,List<Client>> =
+            clientId?.let { actualClientId ->
+                clientRepo.findEnabledClientByUserIdAndClientId(userId, actualClientId)
+                        ?.let { client -> Either.Right(listOf(client)) }
+                        ?: Either.Left(AccessNotFoundException("Unable to find client for User ID $userId and Client ID $clientId"))
+            }
+                    ?: clientRepo.findAllEnabledClientsByUserId(userId)
+                            .let { Either.Right(it) }
+}
+
+private fun createGetRoles(roleRepo: RoleRepository): (Long,Long?) -> Either<Throwable,List<Role>> {
+    return fun (userId: Long, clientId: Long?): Either<Throwable,List<Role>> {
+        return clientId?.let { actualClientId ->
+            roleRepo.findAllByUserIdAndClientId(userId, clientId).let { Either.Right(it) }
+        }
+                ?: roleRepo.findAllByUserId(userId).let { Either.Right(it) }
+    }
+}
+
 @Service
 class AccessLoadingService(
         private val userRepo: UserRepository,
@@ -29,12 +51,19 @@ class AccessLoadingService(
         private val roleRepo: RoleRepository
 ) {
     private val getUserById = createGetUserById(userRepo)
+    private val getClients = createGetClients(clientRepo)
+    private val getRoles = createGetRoles(roleRepo)
+
+    // TODO need to have a client restriction for legacy calls
 
     fun getAccessForUser(userId: Long): Either<Throwable, UserWithClientsAccess> =
+            getAccessForUserAndClient(userId)
+
+    fun getAccessForUserAndClient(userId: Long, clientId: Long? = null): Either<Throwable,UserWithClientsAccess> =
             either.eager {
                 val user = getUserById(userId).bind()
-                val clients = clientRepo.findAllEnabledClientsByUserId(userId)
-                val roles = roleRepo.findAllByUserId(userId)
+                val clients = getClients(userId, clientId).bind()
+                val roles = getRoles(userId, clientId).bind()
 
                 val userClientsMap = clients.associate { client ->
                     val clientRoles = roles.filter { role -> role.clientId == client.id }

e7d4a5789af65d383f2c22e05c9ef88312b7d723
Craig Miller <craigmiller160@gmail.com>
2022-02-21 12:50:13 -0500

    Wrote test setup

--- src/test/kotlin/io/craigmiller160/authserver/entity/ClientUserTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/entity/ClientUserTest.kt
@@ -3,6 +3,8 @@ package io.craigmiller160.authserver.entity
 import io.craigmiller160.authserver.repository.ClientRepository
 import io.craigmiller160.authserver.repository.ClientUserRepository
 import io.craigmiller160.authserver.repository.UserRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,6 +21,45 @@ class ClientUserTest {
     private lateinit var clientRepo: ClientRepository
     @Autowired
     private lateinit var clientUserRepo: ClientUserRepository
+
+    private lateinit var user: User
+    private lateinit var client: Client
+    private lateinit var clientUser: ClientUser
+
+    @BeforeEach
+    fun setup() {
+        user = userRepo.save(User(
+                id = 1,
+                email = "email",
+                firstName = "firstName",
+                lastName = "lastName",
+                password = "password",
+                enabled = true
+        ))
+        client = clientRepo.save(Client(
+                id = 1,
+                name = "name",
+                clientKey = "clientKey",
+                clientSecret = "clientSecret",
+                enabled = true,
+                accessTokenTimeoutSecs = 1,
+                refreshTokenTimeoutSecs = 1,
+                authCodeTimeoutSecs = 1,
+                clientRedirectUris = listOf()
+        ))
+        clientUser = clientUserRepo.save(ClientUser(
+                id = 1,
+                userId = user.id,
+                clientId = client.id
+        ))
+    }
+
+    @AfterEach
+    fun cleanup() {
+        clientUserRepo.deleteAll()
+        clientRepo.deleteAll()
+        userRepo.deleteAll()
+    }
 
     @Test
     fun `able to lazy load joined entities`() {

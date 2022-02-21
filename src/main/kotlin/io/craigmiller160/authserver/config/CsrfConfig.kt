2d1fc5a16ca0d742c89f9c0bfa1f0c59ead94153
Craig Miller <craigmiller160@gmail.com>
2022-02-21 10:53:40 -0500

    Removed CSRF

--- src/main/kotlin/io/craigmiller160/authserver/config/CsrfConfig.kt
+++ src/main/kotlin/io/craigmiller160/authserver/config/CsrfConfig.kt
@@ -23,10 +23,10 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
+//@Configuration
 class CsrfConfig {
 
-    @Bean
+//    @Bean
     fun restCsrfPreventionFilter(): FilterRegistrationBean<RestCsrfPreventionFilter> {
         val filter = RestCsrfPreventionFilter()
         filter.denyStatus = 403

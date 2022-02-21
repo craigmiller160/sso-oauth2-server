9a0f1c3736fcabc5d008a5795a196eb67f4d2aa0
Craig Miller <craigmiller160@gmail.com>
2022-02-21 12:59:09 -0500

    Added more joins to entity and stubbed out new tests

---
+++ src/test/kotlin/io/craigmiller160/authserver/entity/ClientUserRoleTest.kt
@@ -0,0 +1,20 @@
+package io.craigmiller160.authserver.entity
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+class ClientUserRoleTest {
+    @Test
+    fun `able to lazy load joined entities`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `does not cascade deletes`() {
+        TODO("Finish this")
+    }
+}

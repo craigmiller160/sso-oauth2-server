f3a21c989c0ebee4cd4fb3af721c503d47935a46
Craig Miller <craigmiller160@gmail.com>
2022-02-21 15:12:23 -0500

    Cleaned up unnecessary tests

--- src/main/kotlin/io/craigmiller160/authserver/repository/ClientUserRoleRepository.kt
+++ src/main/kotlin/io/craigmiller160/authserver/repository/ClientUserRoleRepository.kt
@@ -20,11 +20,7 @@ package io.craigmiller160.authserver.repository
 
 import io.craigmiller160.authserver.entity.ClientUserRole
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ClientUserRoleRepository : JpaRepository<ClientUserRole,Long> {
-    fun findAllByUserId(userId: Long): List<ClientUserRole>
-}
+interface ClientUserRoleRepository : JpaRepository<ClientUserRole,Long>

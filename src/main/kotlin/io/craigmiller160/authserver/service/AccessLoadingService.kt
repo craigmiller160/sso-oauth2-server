7e5fcbc7d806be72b65b9d4c7f1f5afbbd6facae
Craig Miller <craigmiller160@gmail.com>
2022-02-22 08:29:27 -0500

    Refactored and cleaned up access service

--- src/main/kotlin/io/craigmiller160/authserver/service/AccessLoadingService.kt
+++ src/main/kotlin/io/craigmiller160/authserver/service/AccessLoadingService.kt
@@ -1,69 +1,31 @@
 package io.craigmiller160.authserver.service
 
 import arrow.core.Either
-import arrow.core.computations.ResultEffect.bind
 import arrow.core.computations.either
-import arrow.core.flatMap
 import arrow.core.leftIfNull
 import io.craigmiller160.authserver.dto.access.ClientWithRolesAccess
 import io.craigmiller160.authserver.dto.access.UserWithClientsAccess
-import io.craigmiller160.authserver.entity.Client
-import io.craigmiller160.authserver.entity.Role
-import io.craigmiller160.authserver.entity.User
 import io.craigmiller160.authserver.exception.AccessNotFoundException
 import io.craigmiller160.authserver.repository.ClientRepository
 import io.craigmiller160.authserver.repository.RoleRepository
 import io.craigmiller160.authserver.repository.UserRepository
 import org.springframework.stereotype.Service
 
-private fun createGetUserById(userRepo: UserRepository): (Long) -> Either<Throwable,User> {
-    return fun(userId: Long): Either<Throwable,User> {
-        val userEither = Either.catch { userRepo.findEnabledUserById(userId) }
-                .leftIfNull { AccessNotFoundException("No user for ID: $userId") }
-        return userEither.mapLeft { ex -> AccessNotFoundException("Error querying for user with ID: $userId", ex) }
-    }
-}
-
-private fun createGetClients(clientRepo: ClientRepository): (Long,Long?) -> Either<Throwable,List<Client>> {
-    return fun(userId: Long, clientId: Long?): Either<Throwable,List<Client>> {
-        val clientsEither = clientId?.let { actualClientId ->
-            Either.catch { clientRepo.findEnabledClientByUserIdAndClientId(userId, actualClientId) }
-                    .leftIfNull { AccessNotFoundException("Unable to find client for User ID $userId and Client ID $clientId") }
-                    .map { listOf(it) }
-        }
-                ?: Either.catch { clientRepo.findAllEnabledClientsByUserId(userId) }
-        return clientsEither.mapLeft { ex -> AccessNotFoundException("Error querying for access clients", ex) }
-    }
-}
-
-private fun createGetRoles(roleRepo: RoleRepository): (Long,Long?) -> Either<Throwable,List<Role>> {
-    return fun (userId: Long, clientId: Long?): Either<Throwable,List<Role>> {
-        val rolesEither = clientId?.let { actualClientId ->
-            Either.catch { roleRepo.findAllByUserIdAndClientId(userId, actualClientId) }
-        }
-                ?: Either.catch { roleRepo.findAllByUserId(userId) }
-        return rolesEither.mapLeft { ex -> AccessNotFoundException("Error querying for access roles", ex) }
-    }
-}
 
 @Service
 class AccessLoadingService(
-        userRepo: UserRepository,
-        clientRepo: ClientRepository,
-        roleRepo: RoleRepository
+        private val userRepo: UserRepository,
+        private val clientRepo: ClientRepository,
+        private val roleRepo: RoleRepository
 ) {
-    private val getUserById = createGetUserById(userRepo)
-    private val getClients = createGetClients(clientRepo)
-    private val getRoles = createGetRoles(roleRepo)
 
-    fun getAccessForUser(userId: Long): Either<Throwable, UserWithClientsAccess> =
-            getAccessForUserAndClient(userId)
-
-    fun getAccessForUserAndClient(userId: Long, clientId: Long? = null): Either<Throwable,UserWithClientsAccess> =
-            either.eager {
-                val user = getUserById(userId).bind()
-                val clients = getClients(userId, clientId).bind()
-                val roles = getRoles(userId, clientId).bind()
+    fun getAccessForUser(userId: Long): Either<Throwable,UserWithClientsAccess> =
+            either.eager<Throwable, UserWithClientsAccess> {
+                val user = Either.catch { userRepo.findEnabledUserById(userId) }
+                        .leftIfNull { AccessNotFoundException("Could not find User for ID: $userId") }
+                        .bind()
+                val clients = Either.catch { clientRepo.findAllEnabledClientsByUserId(userId) }.bind()
+                val roles = Either.catch { roleRepo.findAllByUserId(userId) }.bind()
 
                 val userClientsMap = clients.associate { client ->
                     val clientRoles = roles.filter { role -> role.clientId == client.id }
@@ -83,5 +45,6 @@ class AccessLoadingService(
                         clients = userClientsMap
                 )
             }
+                    .mapLeft { ex -> AccessNotFoundException("Error getting access for User with ID: $userId", ex) }
 
 }

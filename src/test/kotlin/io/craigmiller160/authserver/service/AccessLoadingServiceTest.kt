befc7bc4314ceb128ede1f5fa294fb1ee48ad226
Craig Miller <craigmiller160@gmail.com>
2022-04-30 16:54:20 -0400

    Formatting fixes

--- src/test/kotlin/io/craigmiller160/authserver/service/AccessLoadingServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/service/AccessLoadingServiceTest.kt
@@ -161,26 +161,26 @@ class AccessLoadingServiceTest {
 
     val role1 = roleRepo.save(Role(id = 0, clientId = client1.id, name = "TheRole"))
     clientUserRoleRepo.save(
-            ClientUserRole(id = 0, clientId = client1.id, userId = user.id, roleId = role1.id))
+      ClientUserRole(id = 0, clientId = client1.id, userId = user.id, roleId = role1.id))
 
     val role2 = roleRepo.save(Role(id = 0, clientId = client2.id, name = "TheRole2"))
     clientUserRoleRepo.save(
-            ClientUserRole(id = 0, clientId = client2.id, userId = user.id, roleId = role2.id))
+      ClientUserRole(id = 0, clientId = client2.id, userId = user.id, roleId = role2.id))
 
     val result = accessLoadingService.getAccessForUser(user.id)
     result.shouldBeRight(
-            UserWithClientsAccess(
-                    userId = user.id,
-                    email = user.email,
-                    firstName = user.firstName,
-                    lastName = user.lastName,
-                    clients =
-                    mapOf(
-                            client1.clientKey to
-                                    ClientWithRolesAccess(
-                                            clientId = client1.id, clientName = client1.name, roles = listOf(role1.name)),
-                            client2.clientKey to
-                                    ClientWithRolesAccess(
-                                            clientId = client2.id, clientName = client2.name, roles = listOf(role2.name)))))
+      UserWithClientsAccess(
+        userId = user.id,
+        email = user.email,
+        firstName = user.firstName,
+        lastName = user.lastName,
+        clients =
+          mapOf(
+            client1.clientKey to
+              ClientWithRolesAccess(
+                clientId = client1.id, clientName = client1.name, roles = listOf(role1.name)),
+            client2.clientKey to
+              ClientWithRolesAccess(
+                clientId = client2.id, clientName = client2.name, roles = listOf(role2.name)))))
   }
 }

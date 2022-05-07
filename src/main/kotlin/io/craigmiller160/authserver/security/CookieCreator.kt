d977507d5396f6fef77ff67160a6b873f0e24cb6
Craig Miller <craigmiller160@gmail.com>
2022-05-07 17:32:39 -0400

    Added CookieCreator

---
+++ src/main/kotlin/io/craigmiller160/authserver/security/CookieCreator.kt
@@ -0,0 +1,18 @@
+package io.craigmiller160.authserver.security
+
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+object CookieCreator {
+  private const val DEFAULT_MAX_AGE = (24 * 60 * 60).toLong()
+  private val COOKIE_EXP_FORMAT: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss z")
+
+  fun create(cookieName: String, value: String, maxAgeSecs: Long): String {
+    val expires = ZonedDateTime.now(ZoneId.of("GMT")).plusHours(24)
+    val expiresString = COOKIE_EXP_FORMAT.format(expires)
+
+    return "$cookieName=$value; Max-Age=$maxAgeSecs; Expires=$expiresString; Secure; HttpOnly; SameSite=strict;"
+  }
+}

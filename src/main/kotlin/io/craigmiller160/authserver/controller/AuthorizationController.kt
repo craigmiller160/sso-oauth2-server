9b80602ad8e2aec2e05dbba4d112f10ec0d80a69
Craig Miller <craigmiller160@gmail.com>
2022-05-07 15:06:22 -0400

    Stubbed out new endpoint

---
+++ src/main/kotlin/io/craigmiller160/authserver/controller/AuthorizationController.kt
@@ -0,0 +1,16 @@
+package io.craigmiller160.authserver.controller
+
+import io.craigmiller160.authserver.dto.authorization.LoginTokenRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/authorization")
+class AuthorizationController {
+  @PostMapping("/token")
+  fun token(@RequestBody request: LoginTokenRequest) {
+    TODO("Finish this")
+  }
+}

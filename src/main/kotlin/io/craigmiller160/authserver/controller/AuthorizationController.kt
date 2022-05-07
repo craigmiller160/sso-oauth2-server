c9cf8568e0f3492b458dde5209c98a3a62e2ddc7
Craig Miller <craigmiller160@gmail.com>
2022-05-07 15:22:47 -0400

    Cleanup

--- src/main/kotlin/io/craigmiller160/authserver/controller/AuthorizationController.kt
+++ src/main/kotlin/io/craigmiller160/authserver/controller/AuthorizationController.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.authserver.controller
 
 import io.craigmiller160.authserver.dto.authorization.LoginTokenRequest
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/authorization")
 class AuthorizationController {
   @PostMapping("/token")
-  fun token(@RequestBody request: LoginTokenRequest) {
+  fun token(@RequestBody request: LoginTokenRequest): ResponseEntity<*> {
     TODO("Finish this")
   }
 }

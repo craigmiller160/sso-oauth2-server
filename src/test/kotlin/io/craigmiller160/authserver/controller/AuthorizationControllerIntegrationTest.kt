b907f90d4729fc9dfeed33089302da0707def834
Craig Miller <craigmiller160@gmail.com>
2022-05-07 15:09:04 -0400

    Fixed formatting

--- src/test/kotlin/io/craigmiller160/authserver/controller/AuthorizationControllerIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/authserver/controller/AuthorizationControllerIntegrationTest.kt
@@ -1,21 +1,20 @@
-package io.craigmiller160.authserver.controller;
+package io.craigmiller160.authserver.controller
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuthorizationControllerIntegrationTest {
-    @Autowired
-    private lateinit var mockMvc: MockMvc
-    @Test
-    fun token() {
-        TODO("Finish this")
-    }
+  @Autowired private lateinit var mockMvc: MockMvc
+  @Test
+  fun token() {
+    TODO("Finish this")
+  }
 }

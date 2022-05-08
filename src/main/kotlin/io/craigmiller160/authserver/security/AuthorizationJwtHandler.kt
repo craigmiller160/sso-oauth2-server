37561e17915aa513f04d3747131cb8d0e2729e06
Craig Miller <craigmiller160@gmail.com>
2022-05-08 10:07:21 -0400

    Even more refactoring and code cleanup

---
+++ src/main/kotlin/io/craigmiller160/authserver/security/AuthorizationJwtHandler.kt
@@ -0,0 +1,54 @@
+package io.craigmiller160.authserver.security
+
+import arrow.core.Either
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
+import io.craigmiller160.authserver.config.TokenConfig
+import io.craigmiller160.authserver.dto.access.UserWithClientsAccess
+import io.craigmiller160.authserver.dto.access.toClaims
+import io.craigmiller160.authserver.function.TryEither
+import org.springframework.stereotype.Component
+
+@Component
+class AuthorizationJwtHandler(private val tokenConfig: TokenConfig) {
+  companion object {
+    // TODO move these to properties
+    private const val ACCESS_TOKEN_TIMEOUT_SECS = 60 * 10
+    private const val REFRESH_TOKEN_TIMEOUT_SECS = 60 * 60
+  }
+
+  fun createAccessToken(tokenId: String, access: UserWithClientsAccess): TryEither<String> {
+    val claims =
+      JWTClaimsSet.parse(
+        access.toClaims() + createDefaultClaims(tokenId, ACCESS_TOKEN_TIMEOUT_SECS))
+    return createToken(claims)
+  }
+
+  fun createRefreshToken(tokenId: String): TryEither<String> {
+    val claims = JWTClaimsSet.parse(createDefaultClaims(tokenId, REFRESH_TOKEN_TIMEOUT_SECS))
+    return createToken(claims)
+  }
+
+  private fun createDefaultClaims(tokenId: String, expSecs: Int): Map<String, Any> {
+    val now = JwtUtils.generateNow()
+    return mapOf(
+      "iat" to now.time,
+      "exp" to JwtUtils.generateExp(expSecs).time,
+      "jti" to tokenId,
+      "nbf" to now.time)
+  }
+
+  private fun createToken(claims: JWTClaimsSet): TryEither<String> {
+    val header = JWSHeader.Builder(JWSAlgorithm.RS256).build()
+    val jwt = SignedJWT(header, claims)
+    val signer = RSASSASigner(tokenConfig.privateKey)
+
+    return Either.catch {
+      jwt.sign(signer)
+      jwt.serialize()
+    }
+  }
+}
--- src/main/kotlin/io/craigmiller160/authserver/service/AuthorizationService.kt
+++ src/main/kotlin/io/craigmiller160/authserver/service/AuthorizationService.kt
@@ -1,16 +1,8 @@
 package io.craigmiller160.authserver.service
 
 import arrow.core.Either
-import com.nimbusds.jose.JWSAlgorithm
-import com.nimbusds.jose.JWSHeader
-import com.nimbusds.jose.crypto.RSASSASigner
-import com.nimbusds.jwt.JWTClaimsSet
-import com.nimbusds.jwt.SignedJWT
-import io.craigmiller160.authserver.config.TokenConfig
 import io.craigmiller160.authserver.dto.TokenCookieResponse
 import io.craigmiller160.authserver.dto.TokenResponse
-import io.craigmiller160.authserver.dto.access.UserWithClientsAccess
-import io.craigmiller160.authserver.dto.access.toClaims
 import io.craigmiller160.authserver.dto.authorization.LoginTokenRequest
 import io.craigmiller160.authserver.entity.RefreshToken
 import io.craigmiller160.authserver.entity.User
@@ -21,8 +13,8 @@ import io.craigmiller160.authserver.function.rightOrNotFound
 import io.craigmiller160.authserver.function.tryEither
 import io.craigmiller160.authserver.repository.RefreshTokenRepository
 import io.craigmiller160.authserver.repository.UserRepository
+import io.craigmiller160.authserver.security.AuthorizationJwtHandler
 import io.craigmiller160.authserver.security.CookieCreator
-import io.craigmiller160.authserver.security.JwtUtils
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.util.*
@@ -34,17 +26,13 @@ class AuthorizationService(
   private val accessLoadingService: AccessLoadingService,
   private val userRepo: UserRepository,
   private val passwordEncoder: PasswordEncoder,
-  private val tokenConfig: TokenConfig,
-  private val refreshTokenRepo: RefreshTokenRepository
+  private val refreshTokenRepo: RefreshTokenRepository,
+  private val jwtHandler: AuthorizationJwtHandler
 ) {
   companion object {
     private const val ACCESS_TOKEN_COOKIE_NAME = "craigmiller160_access_token"
     private const val REFRESH_TOKEN_COOKIE_NAME = "craigmiller160_refresh_token"
     private const val REFRESH_TOKEN_COOKIE_PATH = "/authserver/authentication/refresh"
-
-    // TODO move these to properties
-    private const val ACCESS_TOKEN_TIMEOUT_SECS = 60 * 10
-    private const val REFRESH_TOKEN_TIMEOUT_SECS = 60 * 60
   }
 
   fun token(
@@ -56,8 +44,8 @@ class AuthorizationService(
 
       val tokenId = UUID.randomUUID().toString()
 
-      val accessToken = createAccessToken(tokenId, access).bind()
-      val refreshToken = createRefreshToken(tokenId).bind()
+      val accessToken = jwtHandler.createAccessToken(tokenId, access).bind()
+      val refreshToken = jwtHandler.createRefreshToken(tokenId).bind()
       saveRefreshToken(refreshToken, tokenId, user.id).bind()
 
       if (request.cookie) {
@@ -81,38 +69,6 @@ class AuthorizationService(
     val refreshTokenEntity =
       RefreshToken(tokenId, refreshToken, null, userId, ZonedDateTime.now(ZoneId.of("UTC")))
     return Either.catch { refreshTokenRepo.save(refreshTokenEntity) }
-  }
-
-  private fun createRefreshToken(tokenId: String): TryEither<String> {
-    val claims = JWTClaimsSet.parse(createDefaultClaims(tokenId, REFRESH_TOKEN_TIMEOUT_SECS))
-    return createToken(claims)
-  }
-
-  private fun createAccessToken(tokenId: String, access: UserWithClientsAccess): TryEither<String> {
-    val claims =
-      JWTClaimsSet.parse(
-        access.toClaims() + createDefaultClaims(tokenId, ACCESS_TOKEN_TIMEOUT_SECS))
-    return createToken(claims)
-  }
-
-  private fun createToken(claims: JWTClaimsSet): TryEither<String> {
-    val header = JWSHeader.Builder(JWSAlgorithm.RS256).build()
-    val jwt = SignedJWT(header, claims)
-    val signer = RSASSASigner(tokenConfig.privateKey)
-
-    return Either.catch {
-      jwt.sign(signer)
-      jwt.serialize()
-    }
-  }
-
-  private fun createDefaultClaims(tokenId: String, expSecs: Int): Map<String, Any> {
-    val now = JwtUtils.generateNow()
-    return mapOf(
-      "iat" to now.time,
-      "exp" to JwtUtils.generateExp(expSecs).time,
-      "jti" to tokenId,
-      "nbf" to now.time)
   }
 
   private fun validateCredentials(request: LoginTokenRequest): TryEither<User> =

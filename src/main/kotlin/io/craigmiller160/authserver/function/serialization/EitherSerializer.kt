dee9280a06f58d656234aa414c54e6e3bee440a2
Craig Miller <craigmiller160@gmail.com>
2022-05-15 12:32:03 -0400

    Progress

--- src/main/kotlin/io/craigmiller160/authserver/function/serialization/EitherSerializer.kt
+++ src/main/kotlin/io/craigmiller160/authserver/function/serialization/EitherSerializer.kt
@@ -4,7 +4,6 @@ import arrow.core.Either
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
-import io.craigmiller160.authserver.exception.InvalidLoginException
 
 class EitherSerializer : StdSerializer<Either<*, *>>(Either::class.java) {
   override fun serialize(
@@ -12,6 +11,9 @@ class EitherSerializer : StdSerializer<Either<*, *>>(Either::class.java) {
     jsonGenerator: JsonGenerator?,
     provider: SerializerProvider?
   ) {
-    throw InvalidLoginException("Special Dying")
+    when (either) {
+      is Either.Right<*> -> TODO("Figure out how to automatically serialize value")
+      else -> TODO("Throw exception")
+    }
   }
 }

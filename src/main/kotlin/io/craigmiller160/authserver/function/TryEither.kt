16ccb57ce5fe0849c2f6afee83d289831d3d978e
Craig Miller <craigmiller160@gmail.com>
2022-05-15 19:23:47 -0400

    Working on either error handling

--- src/main/kotlin/io/craigmiller160/authserver/function/TryEither.kt
+++ src/main/kotlin/io/craigmiller160/authserver/function/TryEither.kt
@@ -3,11 +3,13 @@ package io.craigmiller160.authserver.function
 import arrow.core.Either
 import arrow.core.computations.either
 import io.craigmiller160.authserver.exception.NotFoundException
+import org.springframework.http.ResponseEntity
 
 typealias TryEither<T> = Either<Throwable, T>
 
 typealias TryEitherCompanion = Either.Companion
 
+// TODO can this be renamed back to the original one?
 object runTryEither {
   inline fun <T> eager(
     crossinline c: suspend arrow.core.computations.RestrictedEitherEffect<Throwable, *>.() -> T
@@ -16,3 +18,11 @@ object runTryEither {
 
 fun <T> TryEitherCompanion.rightOrNotFound(value: T?, itemName: String = ""): TryEither<T> =
   value?.let { Either.Right(it) } ?: Either.Left(NotFoundException("$itemName Not Found"))
+
+fun <T> TryEither<T>.toResponseEntity(
+  builder: (ResponseEntity.BodyBuilder) -> ResponseEntity.BodyBuilder
+) =
+  when (this) {
+    is Either.Right<T> -> ResponseEntity.status(200).apply { builder(this) }.body(value)
+    is Either.Left<Throwable> -> ResponseEntity.status(500).build()
+  }

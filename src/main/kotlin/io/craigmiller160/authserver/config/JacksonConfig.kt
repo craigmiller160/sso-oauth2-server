69efde7ab86c9b755fbe4f4c87cc898b2dbfcc12
Craig Miller <craigmiller160@gmail.com>
2022-05-15 12:14:20 -0400

    Tried getting arrow module to solve my problems

---
+++ src/main/kotlin/io/craigmiller160/authserver/config/JacksonConfig.kt
@@ -0,0 +1,14 @@
+package io.craigmiller160.authserver.config
+
+import arrow.integrations.jackson.module.registerArrowModule
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
+
+@Configuration
+class JacksonConfig {
+  @Bean
+  fun objectMapper(builder: Jackson2ObjectMapperBuilder): ObjectMapper =
+    builder.build<ObjectMapper>().registerArrowModule()
+}

b6eb047526c644ad9b90418cc801a7977dd58978
Craig Miller <craigmiller160@gmail.com>
2022-06-07 15:45:45 -0400

    Beginning to implement gradle functionality

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,15 @@
 group = "io.craigmiller160"
 version = "1.8.0-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    application
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://craigmiller160.ddns.net:30003/repository/maven-public")
+    }
+}

ce7d6c4089135f29c2b21923459e87d19a789ae2
Craig Miller <craigmiller160@gmail.com>
2022-06-12 09:01:17 -0400

    Fixing dependency refresh settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import com.diffplug.gradle.spotless.SpotlessExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import java.time.temporal.ChronoUnit
 
 group = "io.craigmiller160"
 version = "1.8.0-SNAPSHOT"
@@ -15,6 +17,12 @@ plugins {
     id("com.diffplug.spotless") version "6.6.1"
 }
 apply(plugin = "io.spring.dependency-management")
+
+the<DependencyManagementExtension>().apply {
+    resolutionStrategy {
+        cacheChangingModulesFor(0, TimeUnit.SECONDS)
+    }
+}
 
 publishing {
     publications {

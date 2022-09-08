17b2c9e12e417a9ce614cd9cb933caa15e120576
Craig Miller <craigmiller160@gmail.com>
2022-09-08 16:03:28 -0400

    Changed the repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
     repositories {
         maven {
             val repo = if (project.version.toString().endsWith("-SNAPSHOT")) "maven-snapshots" else "maven-releases"
-            url = uri("https://craigmiller160.ddns.net:30003/repository/$repo")
+            url = uri("https://nexus-craigmiller160.ddns.net/repository/$repo")
             credentials {
                 username = System.getenv("NEXUS_USER")
                 password = System.getenv("NEXUS_PASSWORD")
@@ -49,7 +49,7 @@ publishing {
 repositories {
     mavenCentral()
     maven {
-        url = uri("https://craigmiller160.ddns.net:30003/repository/maven-public")
+        url = uri("https://nexus-craigmiller160.ddns.net/repository/maven-public")
     }
 }
 
